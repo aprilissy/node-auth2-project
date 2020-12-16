ccde03bada489a63a09634d1e4f8077856c55353
April Darger <april.m.darger@gmail.com>
2020-12-15 18:37:47 -0700

    Fleshing out endpoints with model, secret, middleware validation.

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,19 +1,47 @@
 const router = require('express').Router();
-const jwt = require('jsonwebtoken');
 const bcryptjs = require('bcryptjs');
-const { jwtSecret } = require('../../config/secrets');
 
 const Users = require('../users/users-model');
+const { isValid, newToken } = require('../middleware/middleware');
 
 router.post('/register', (req, res) => {
-// Creates a user using the information sent inside the body of the request. 
-// Hash the password before saving the user to the database.
+  const credentials = req.body;
+  if(isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+    credentials.password = hash;
+    Users.add(credentials)
+      .then(user => {
+        res.status(201).json({ data: user });
+      });
+  } else {
+    res.status(400).json({ message: `please provide username and password. The password should be a combination of letters and numbers` });
+  }
 });
 
-router.post('/login', (req, res) => {
-// Use the credentials sent inside the body to authenticate the user. 
-// On successful login, create a new JWT with the user id as the subject and send it back to the client. 
-// If login fails, respond with the correct status code and the message: 'You shall not pass!'
+router.post('/login', (req, res) => { 
+// On successful login, create a new JWT with the user id as the subject and send it back to the client.
+  const {username, password} = req.body;
+  if(isValid(req.body)) {
+    Users.getBy({ username:username })
+      .then(([user]) => {
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = newToken(user);
+          res.status(200).json({
+            message:`Welcome ${user.username}`,
+            token
+          });
+        } else {
+          res.status(401).json({ message: `You shall not pass!` });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message });
+      });
+  } else {
+    res.status(400).json({ message: `You shall not pass!` });
+  }
+
 });
 
 module.exports = router;
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -7,12 +7,11 @@ module.exports = {
 };
 
 async function add(user) {
-  const [id] = await db('users')
-    .insert(user, 'id');
-    return db ('users')
-      .select('users.id', 'users.username', 'users.department')
-      .where('users.id', id)
-      .first();
+  const [id] = await db('users').insert(user, 'id');  
+  return db ('users')
+    .select('users.id', 'users.username', 'users.department')
+    .where('users.id', id)
+    .first();
 }
 
 function getAll() {
@@ -23,4 +22,5 @@ function getAll() {
 function getBy(filter) {
   return db ('users')
     .select('users.id', 'users.username', 'users.department', 'users.password')
+    .where(filter);
 }
